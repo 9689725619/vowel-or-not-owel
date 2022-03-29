64010dab76ec97049c16aa7e13f3cdbfc011ecab
Shubhangi meshram <101330420+9689725619@users.noreply.github.com>
2022-03-29 17:23:45 +0530

    Add files via upload

---
+++ vowel or not.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+
+char ch;
+
+printf("enter any charecter :");
+scanf("%c",&ch);
+if (ch=='a' || ch=='e' || ch=='i' || ch=='o' 
+||  ch=='u');
+
+
+{
+	printf("charecter is vowel",ch);
+}
+else
+{
+	printf("not vowel"ch);
+}
+return 0;
+}
